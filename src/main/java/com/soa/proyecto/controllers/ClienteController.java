861c32b46b1c721ab8aab2adeee89f962c3c0af1
Richard Carrion <rcarrion3406@gmail.com>
2022-02-03 22:23:11 -0500

    thymeleaf test

--- src/main/java/com/soa/proyecto/controllers/ClienteController.java
+++ src/main/java/com/soa/proyecto/controllers/ClienteController.java
@@ -5,27 +5,24 @@ import com.soa.proyecto.servicios.ClienteServicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping(value = "api/cliente")
+@Controller
 public class ClienteController {
+
     @Autowired
     private ClienteServicios clienteServicios;
-    @GetMapping
-    public ResponseEntity<?> getAll(){
-        Map<String, Object> response = new HashMap<>();
+
+    @GetMapping("clientes")
+    public String getAll(Model model){
         List<Cliente> lista = clienteServicios.get();
-        if (lista.size() > 0) {
-            return new ResponseEntity<List<Cliente>>(lista, HttpStatus.OK);
-        }
-        response.put("mensaje", "No hay estudiantes en la base de datos.");
-        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        model.addAttribute("clientes", lista);
+        return "clientes";
     }
 }
